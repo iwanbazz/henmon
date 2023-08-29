c74f658afee1eb1f76e681e5e161d421fd47488b
iwanbazz <iwan.bazz@gmail.com>
2023-08-29 22:04:38 +0700

    update

--- henmon.rb
+++ henmon.rb
@@ -39,54 +39,58 @@ end
 name = ARGV[0]
 time = ARGV[1]
 condition = ARGV[2]
-puts "monitoring #{name} scraper starting every #{time}"
-puts condition
+if name.nil?
+  puts "usage: henmon scraper_name time_to_watch"
+  puts "example: henmon spydeals_nl 15m"
+else
+  puts "monitoring #{name} scraper starting every #{time}"
+  puts condition
+  scheduler.every time do
+    result = check_hen_scraper(name)
+    puts(result['scraper_name'])
+    puts "job_id: #{result['job_id']}"
+    puts "job_status: #{result['job_status']}"
+    puts "to_fetch: #{result['to_fetch']}"
+    puts "pages: #{result['pages']}"
+    puts "fetching_failed: #{result['fetching_failed']}"
+    puts "parsing_failed: #{result['parsing_failed']}"
+    puts "refetch_failed: #{result['refetch_failed']}"
+    puts "limbo: #{result['limbo']}"
+    puts "outputs: #{result['outputs']}"
+    puts "time_stamp: #{result['time_stamp']}"
+    puts '----------------------------------------'
 
-scheduler.every time do
-  result = check_hen_scraper(name)
-  puts(result['scraper_name'])
-  puts "job_id: #{result['job_id']}"
-  puts "job_status: #{result['job_status']}"
-  puts "to_fetch: #{result['to_fetch']}"
-  puts "pages: #{result['pages']}"
-  puts "fetching_failed: #{result['fetching_failed']}"
-  puts "parsing_failed: #{result['parsing_failed']}"
-  puts "refetch_failed: #{result['refetch_failed']}"
-  puts "limbo: #{result['limbo']}"
-  puts "outputs: #{result['outputs']}"
-  puts "time_stamp: #{result['time_stamp']}"
-  puts '----------------------------------------'
-
-  if result['job_status'] == 'paused' && (result['fetching_failed']).positive?
-    refetch = refetch(name)
-    if refetch['status'] == 'to_process'
-      resume_start = resume(name)
-      puts "Refetch then resume: #{resume_start}"
-    end
-  elsif result['job_status'] == 'paused' && result['fetching_failed'].zero? && (result['refetch_failed']).positive?
-    reparse_failed = reparse_on_failed(name)
-    if reparse_failed['status'] == 'to_process'
-      resume_start = resume(name)
-      puts "Reparse then resume: #{resume_start}"
-    end
-  elsif result['job_status'] == 'paused' && result['fetching_failed'].zero?
-    resume_start = resume(name)
-    puts "Resume: #{resume_start}"
-  elsif (result['fetching_failed']).positive?
-    if condition.nil?
+    if result['job_status'] == 'paused' && (result['fetching_failed']).positive?
       refetch = refetch(name)
-      puts "Refetch status: #{refetch}"
+      if refetch['status'] == 'to_process'
+        resume_start = resume(name)
+        puts "Refetch then resume: #{resume_start}"
+      end
+    elsif result['job_status'] == 'paused' && result['fetching_failed'].zero? && (result['refetch_failed']).positive?
+      reparse_failed = reparse_on_failed(name)
+      if reparse_failed['status'] == 'to_process'
+        resume_start = resume(name)
+        puts "Reparse then resume: #{resume_start}"
+      end
+    elsif result['job_status'] == 'paused' && result['fetching_failed'].zero?
+      resume_start = resume(name)
+      puts "Resume: #{resume_start}"
+    elsif (result['fetching_failed']).positive?
+      if condition.nil?
+        refetch = refetch(name)
+        puts "Refetch status: #{refetch}"
+      end
+    elsif (result['parsing_failed']).positive?
+      unless condition.nil?
+        refetch_parse = refetch_parse(name)
+        puts "Refetch status: #{refetch_parse}"
+      end
+    elsif result['job_status'] == 'done'
+      puts `Scraper done at: #{result['time_stamp']}`
+      scheduler.shutdown
     end
-  elsif (result['parsing_failed']).positive?
-    unless condition.nil?
-      refetch_parse = refetch_parse(name)
-      puts "Refetch status: #{refetch_parse}"
-    end
-  elsif result['job_status'] == 'done'
-    puts `Scraper done at: #{result['time_stamp']}`
-    scheduler.shutdown
   end
-end
 
-# Keep the script running
-scheduler.join
+  # Keep the script running
+  scheduler.join
+end
